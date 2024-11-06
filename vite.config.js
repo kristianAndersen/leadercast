91fb32a0f8829184352c82d1b1839802cc5e459c
kristian andersen <krilleandersen@gmail.com>
2024-11-06 14:51:18 +0100

    changec vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import * as path from 'path';
+import path from 'path'
 import fs from 'fs';
 // https://vite.dev/config/
 import { fileURLToPath } from 'node:url';
+
 var __dirname = path.dirname(fileURLToPath(import.meta.url));
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+      
+  const isProduction = mode === 'production';
+
+  return {
     plugins: [vue()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
-    server: {
+    server: isProduction ? {} : {
         https: {
             key: fs.readFileSync("".concat(__dirname, "/cert/key.pem")),
             cert: fs.readFileSync("".concat(__dirname, "/cert/cert.pem"))
         },
         proxy: {}
     }
+  };
 });
